72df07cf3ff6fbb15d107bf1cc45ed33abe6c6b7
Daniel  Cruz <danielcruz489@gmail.com>
2020-12-10 13:37:35 -0700

    added js for theme change and local storage

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log("It's working");
+
+//Get's current theme set in local storage
+let theme = localStorage.getItem("theme");
+
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("Option Clicked:", mode);
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "default.css";
+  }
+
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+
+  //   Sets theme color in local storage so color stays after page refresh
+  localStorage.setItem("theme", mode);
+}
